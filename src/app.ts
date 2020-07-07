873652ccd4c1fba8f25881bb42280ee61a308738
Mas <mas@mas.mas>
2020-07-07 14:08:20 +0300

    config making issues, deleting

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import app from "./preApp";
-import { appEnvConfig } from "./base/loaders/baseLoader";
+// import { appEnvConfig } from "./base/loaders/baseLoader";
 import { testCtr } from "./modules/test/controller/testCtr";
 
 console.log("--------", process.env.ENV);
 app.get("/test", testCtr);
 
-app.listen(appEnvConfig.port, () => {
-  console.log(`app is ready on port: ${appEnvConfig.port}`);
+app.listen("4321", () => {
+  console.log(`app is ready on port: ${"4321"}`);
 });
--- src/base/loaders/baseLoader.js
+++ src/base/loaders/baseLoader.js
@@ -1,6 +1,6 @@
-import configs from "../../../configs/config.js";
-
-const env = process.env.ENV;
-if (env) env.trim();
-export const appEnv = env;
-export const appEnvConfig = configs.default.getEnvObj(appEnv);
+// import configs from "../../configs/config";
+//
+// const env = process.env.ENV;
+// if (env) env.trim();
+// export const appEnv = env;
+// export const appEnvConfig = configs.default.getEnvObj(appEnv);
